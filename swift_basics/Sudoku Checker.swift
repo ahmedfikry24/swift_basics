41795466835e73dad973c4ad5ad6a40d65308965
ahmed fikry <ahmedfikryelshimi24@gmail.com>
2025-08-14 02:02:26 +0300

    check rows, columns and structure

--- swift_basics/Sudoku Checker.swift
+++ swift_basics/Sudoku Checker.swift
@@ -1,2 +1,59 @@
 
 import Foundation
+
+class SudokuChecker {
+    
+    func isValidStructure(sudoku: [[String]]) -> Bool {
+        return sudoku.count == sudoku[0].count && sudoku.count >= 9
+    }
+    
+    
+    
+    func isRowsValid(sudoku: [[String]] ) -> Bool {
+        var rowSet = Set<String>()
+        var isValid = true
+        
+        sudoku.forEach{row in
+            row.forEach {  number in
+                if rowSet.contains(number) && number != "_"{
+                  isValid = false
+                    return
+                } else{ rowSet.insert(number) }
+            }
+            rowSet.removeAll()
+            if !isValid { return }
+        }
+        
+        return isValid
+    }
+    
+    func isColumnsValid(sudoku: [[String]] ) -> Bool {
+        var columnSet = Set<String>()
+        var currentColumnPosition = 0
+        var isValid = true
+        
+        for _ in 1...sudoku.count {
+            
+            sudoku.forEach{ row in
+                let currentValue = row[currentColumnPosition]
+                
+                if columnSet.contains(currentValue)  && currentValue != "_" {
+                    isValid = false
+                    return
+                }else{columnSet.insert(currentValue)}
+            }
+            currentColumnPosition += 1
+            columnSet.removeAll()
+            if !isValid { break }
+        }
+
+        return isValid
+    }
+    
+    
+    
+}
+
+
+
+
--- swift_basics/main.swift
+++ swift_basics/main.swift
@@ -1,3 +1,22 @@
 
 import Foundation
 
+let sudokuChecker = SudokuChecker()
+
+let sudoku = [
+    ["1","3","_" ,"_","2","5","_" ,"_","7"],
+    ["6","2","_" ,"_","9","7","_" ,"_","8"],
+    ["2","1","_" ,"_","3","6","_" ,"_","5"],
+    ["8","4","_" ,"_","6","4","_" ,"_","2"],
+    ["9","5","_" ,"_","8","2","_" ,"_","6"],
+    ["4","6","_" ,"_","5","3","_" ,"_","1"],
+    ["7","8","_" ,"_","4","1","_" ,"_","9"],
+    ["3","9","_" ,"_","7","8","_" ,"_","4"],
+    ["5","7","_" ,"_","1","9","_" ,"_","3"],
+]
+
+
+
+print("this sudoku structure is \(sudokuChecker.isValidStructure(sudoku: sudoku) ? "valid" : "invalid")")
+print("this sudoku rows is \(sudokuChecker.isRowsValid(sudoku: sudoku) ? "valid" : "invalid")")
+print("this sudoku columns is \(sudokuChecker.isColumnsValid(sudoku: sudoku) ? "valid" : "invalid")")
