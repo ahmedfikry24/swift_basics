d8f0a36a61e24990ad3416586119e78a54bdb635
ahmed fikry <ahmedfikryelshimi24@gmail.com>
2025-08-14 18:12:47 +0300

    finsh sudoku checker rows, columns && subgrids

--- swift_basics/Sudoku Checker.swift
+++ swift_basics/Sudoku Checker.swift
@@ -3,10 +3,48 @@ import Foundation
 
 class SudokuChecker {
     
-    func isValidStructure(sudoku: [[String]]) -> Bool {
-        return sudoku.count == sudoku[0].count && sudoku.count >= 9
+    private let validNumbers = ["_","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    func isSubgridsValid(sudoku: [[String]] ) -> Bool {
+        var isValid = true
+        let subgridCount = 3
+        
+        for _ in 1...subgridCount {
+            var currentStage = 0
+            
+            let currentRows = sudoku.dropFirst(currentStage).prefix(subgridCount)
+            
+            for _ in 1...subgridCount{
+            
+                var currentRowStage = 0
+                
+                var subgrid = Array<[String]>()
+                currentRows.forEach { row in
+                    let rows = Array(row.dropFirst(currentRowStage).prefix(subgridCount))
+                    subgrid.append(rows)
+                }
+                
+               var sudgridValues = currentRows.flatMap{ $0 }
+                Set(sudgridValues).count == 9 ? () : (isValid = false)
+               sudgridValues.removeAll()
+                
+                if !isValid { break }
+                currentRowStage += 3
+            }
+            
+            if !isValid { break }
+            currentStage += 3
+        }
+        
+        return isValid
     }
     
+    
+    
+    
+    func isValidStructure(sudoku: [[String]]) -> Bool {
+        return sudoku.count == sudoku[0].count && sudoku.count == 9
+    }
     
     
     func isRowsValid(sudoku: [[String]] ) -> Bool {
@@ -14,8 +52,9 @@ class SudokuChecker {
         var isValid = true
         
         sudoku.forEach{row in
-            row.forEach {  number in
-                if rowSet.contains(number) && number != "_"{
+            row.forEach {
+                let number = $0.trimmingCharacters(in: .whitespacesAndNewlines)
+                if rowSet.contains(number) && number != "_" || !isValidNumber(number: number){
                   isValid = false
                     return
                 } else{ rowSet.insert(number) }
@@ -35,9 +74,9 @@ class SudokuChecker {
         for _ in 1...sudoku.count {
             
             sudoku.forEach{ row in
-                let currentValue = row[currentColumnPosition]
+                let currentValue = row[currentColumnPosition].trimmingCharacters(in: .whitespacesAndNewlines)
                 
-                if columnSet.contains(currentValue)  && currentValue != "_" {
+                if columnSet.contains(currentValue)  && currentValue != "_" || !isValidNumber(number: currentValue) {
                     isValid = false
                     return
                 }else{columnSet.insert(currentValue)}
@@ -50,7 +89,9 @@ class SudokuChecker {
         return isValid
     }
     
-    
+    private func isValidNumber(number: String) -> Bool {
+        return validNumbers.contains(number)
+    }
     
 }
 
--- swift_basics/main.swift
+++ swift_basics/main.swift
@@ -4,10 +4,10 @@ import Foundation
 let sudokuChecker = SudokuChecker()
 
 let sudoku = [
-    ["1","3","_" ,"_","2","5","_" ,"_","7"],
+    ["1","4","_" ,"_","2","5","_" ,"_","7"],
     ["6","2","_" ,"_","9","7","_" ,"_","8"],
     ["2","1","_" ,"_","3","6","_" ,"_","5"],
-    ["8","4","_" ,"_","6","4","_" ,"_","2"],
+    ["8","3","_" ,"_","6","4","_" ,"_","2"],
     ["9","5","_" ,"_","8","2","_" ,"_","6"],
     ["4","6","_" ,"_","5","3","_" ,"_","1"],
     ["7","8","_" ,"_","4","1","_" ,"_","9"],
@@ -20,3 +20,4 @@ let sudoku = [
 print("this sudoku structure is \(sudokuChecker.isValidStructure(sudoku: sudoku) ? "valid" : "invalid")")
 print("this sudoku rows is \(sudokuChecker.isRowsValid(sudoku: sudoku) ? "valid" : "invalid")")
 print("this sudoku columns is \(sudokuChecker.isColumnsValid(sudoku: sudoku) ? "valid" : "invalid")")
+print("this sudoku subgrid is \(sudokuChecker.isSubgridsValid(sudoku: sudoku) ? "valid" : "invalid")")
